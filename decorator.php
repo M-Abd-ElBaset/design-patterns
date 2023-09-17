df92c2d49a3aef20725307ba8f1bb278034dc14e
Mohamed Elgebaly <36784871+M-Abd-ElBaset@users.noreply.github.com>
2023-09-17 15:50:24 +0300

    add decorator

---
+++ decorator.php
@@ -0,0 +1,46 @@
+<?php
+
+interface CarService
+{
+    public function getCost();
+}
+
+class BasicInspection implements CarService
+{
+    public function getCost()
+    {
+        return 19;
+    }
+}
+
+class OilExchange implements CarService
+{
+    protected CarService $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+        return 25 + $this->carService->getCost();
+    }
+}
+
+class TireRotation implements CarService
+{
+    protected CarService $carService;
+
+    public function __construct(CarService $carService)
+    {
+        $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+        return 15 + $this->carService->getCost();
+    }
+}
+
+echo (new TireRotation(new OilExchange(new BasicInspection())))->getCost();
